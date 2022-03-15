4f48039eab4e8ff484ca27cc9912133be5bfd2dd
sinaenjuni <y2k4388@gmail.com>
2022-03-15 19:52:22 +0900

    Training LSGAN and ResNet32 through the pre-trained LSGAN

--- src/GAN/resnet_s_with_GAN(LSGAN).py
+++ src/GAN/resnet_s_with_GAN(LSGAN).py
@@ -28,8 +28,14 @@ print('device:', device)
 
 
 # Define hyper-parameters
-name = 'experiments2/Resnet_s/LSGAN'
-tensorboard_path = f'../../tb_logs/{name}'
+name = 'LSGAN/cifar10/aug/p100'
+TENSORBOARD_PATH = f'../../tb_logs/{name}/'
+
+# Save the model checkpoints
+SAVE_PATH = f'../../weights/{name}/'
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+
 
 num_workers = 4
 num_epochs = 200
@@ -61,7 +67,7 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 # Define Tensorboard
-tb = getTensorboard(tensorboard_path)
+tb = getTensorboard(TENSORBOARD_PATH)
 
 
 
@@ -135,36 +141,6 @@ g_optimizer = torch.optim.Adam(G.parameters(), lr=learning_rate, betas=(beta1, b
 
 
 
-def compute_gradient_penalty(D, real_samples, fake_samples):
-    # real_samples = real_samples.reshape(real_samples.size(0), 1, 32, 32).to(device)
-    # fake_samples = fake_samples.reshape(fake_samples.size(0), 1, 32, 32).to(device)
-
-    """Calculates the gradient penalty loss for WGAN GP"""
-    # Random weight term for interpolation between real and fake samples
-    alpha = torch.rand(real_samples.size(0), 1, 1, 1).to(device)
-    # Get random interpolation between real and fake samples
-    interpolates = (alpha * real_samples.data + ((1 - alpha) * fake_samples.data)).requires_grad_(True)
-    # d_interpolates = D(interpolates.reshape(real_samples.size(0), -1))
-    d_interpolates = D(interpolates)
-
-    weights = torch.ones(d_interpolates.size()).to(device)
-    # Get gradient w.r.t. interpolates
-    gradients = torch.autograd.grad(
-        outputs=d_interpolates,
-        inputs=interpolates,
-        grad_outputs=weights,
-        create_graph=True,
-        retain_graph=True,
-        only_inputs=True,
-    )[0]
-
-    gradients = gradients.view(gradients.size(0), -1)
-    gradients2L2norm = torch.sqrt(torch.sum(gradients ** 2, dim=1))
-    gradient_penalty = torch.mean(( gradients2L2norm - 1 ) ** 2)
-    return gradient_penalty
-
-
-
 # Training model
 total_step = len(train_data_loader)
 for epoch in range(num_epochs):
@@ -177,70 +153,50 @@ for epoch in range(num_epochs):
         real_labels = torch.ones((batch,) ).to(device)
         fake_labels = torch.zeros((batch,) ).to(device)
 
-        # Labels shape is (batch_size, 1): [batch_size, 1]
+        # ================================================================== #
+        #                        Train the generator                         #
+        # ================================================================== #
+        z = torch.randn(batch, nz, 1, 1).to(device)
+
+        g_optimizer.zero_grad()
+
+        fake_images = G(z)
+        g_outputs = D(fake_images).view(batch, -1)
+
+        g_loss = torch.mean((g_outputs - real_labels) ** 2)
+        score_G = g_outputs.mean().item()
+
+        g_loss.backward()
+        g_optimizer.step()
+
         # ================================================================== #
         #                      Train the discriminator                       #
         # ================================================================== #
         d_optimizer.zero_grad()
 
         real_output = D(images).view(batch, -1)
-        # d_loss_real = criterion(outputs, real_labels)
-        # loss_D_real = F.relu(1.-outputs).mean()
-        # loss_D_real = -(real_output.mean())
-        loss_D_real = torch.mean((real_output - 1) ** 2)
-        score_D_real = real_output.mean().item()
+        d_real_loss = 0.5 * torch.mean((real_output - real_labels) ** 2)
+        d_real_score = real_output.mean().item()
 
-        # Compute BCELoss using fake images
-        # First term of the loss is always zero since fake_labels == 0
-        # z = torch.randn(batch_size, latent_size).to(device) # mean==0, std==1
         z = torch.randn(batch, nz, 1, 1).to(device) # mean==0, std==1
         fake_images = G(z)
 
         fake_output = D(fake_images.detach()).view(batch, -1)
-        # d_loss_fake = criterion(outputs, fake_labels)
-        # loss_D_fake = F.relu(1.+outputs).mean()
-        # loss_D_fake = fake_output.mean()
-        loss_D_fake = torch.mean(fake_output ** 2)
-        score_D_fake = fake_output.mean().item()
+        d_fake_loss = 0.5 * torch.mean(fake_output - fake_labels ** 2)
+        d_fake_score = fake_output.mean().item()
 
-        # Backprop and optimize
-        gradient_penalty = compute_gradient_penalty(D, images.data, fake_images.data)
         # d_loss = loss_D_real + loss_D_fake + lambda_gp * gradient_penalty
-        d_loss = 0.5 * (loss_D_real + loss_D_fake).mean() + lambda_gp * gradient_penalty
+        d_loss = 0.5 * (d_real_loss + d_fake_loss)
 
         # reset_grad()
         d_loss.backward()
         d_optimizer.step()
 
-        # ================================================================== #
-        #                        Train the generator                         #
-        # ================================================================== #
-
-        if i % n_critic == 0:
-            g_optimizer.zero_grad()
-
-            # Compute loss with fake images
-            z = torch.randn(batch, nz, 1, 1).to(device)
-            fake_images = G(z)
-            outputs = D(fake_images).view(batch, -1)
-            # g_loss = -outputs.mean()
-            g_loss = torch.mean((outputs - 1) ** 2)
-            score_G = outputs.mean().item()
-
-            # We train G to maximize log(D(G(z)) instead of minimizing log(1-D(G(z)))
-            # For the reason, see the last paragraph of section 3. https://arxiv.org/pdf/1406.2661.pdf
-            # g_loss = criterion(outputs, real_labels)
-
-            # Backprop and optimize
-            # reset_grad()
-            g_loss.backward()
-            g_optimizer.step()
-
         if (i + 1) % 10 == 0:
             print('Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f} / {:.2f}'
                   .format(epoch+1, num_epochs, i + 1, total_step,
                           d_loss.item(), g_loss.item(),
-                          score_D_real, score_D_fake, score_G))
+                          d_real_score, d_fake_score, score_G))
 
     # Save real images
     # if (epoch + 1) == 1:
@@ -254,8 +210,8 @@ for epoch in range(num_epochs):
 
     tb.add_scalars(global_step=epoch + 1,
                    main_tag='score',
-                   tag_scalar_dict={'real':score_D_real,
-                                    'fake':score_D_fake,
+                   tag_scalar_dict={'real':d_real_score,
+                                    'fake':d_fake_score,
                                     'g':score_G})
 
     # tb.add_scalar(tag='d_loss', global_step=epoch+1, scalar_value=d_loss.item())
@@ -278,10 +234,7 @@ for epoch in range(num_epochs):
     # save_image(denorm(fake_images), os.path.join(sample_dir, 'fake_images-{}.png'.format(epoch + 1)))
 
 
-    # Save the model checkpoints
-    SAVE_PATH = f'../../weights/{name}/'
-    if not os.path.exists(SAVE_PATH):
-        os.makedirs(SAVE_PATH)
+
     torch.save(G.state_dict(), SAVE_PATH + f'G_{epoch+1}.pth')
     torch.save(D.state_dict(), SAVE_PATH + f'D_{epoch+1}.pth')
 
